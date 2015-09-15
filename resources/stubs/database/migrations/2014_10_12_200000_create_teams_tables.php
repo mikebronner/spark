47349881d93ecee92bc7d16619917ce066a1c81e
Mike Bronner <mike@genealabs.com>
2015-09-15 15:45:09 -0700

    Add referential itegrity to migrations

--- resources/stubs/database/migrations/2014_10_12_200000_create_teams_tables.php
+++ resources/stubs/database/migrations/2014_10_12_200000_create_teams_tables.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
 
 class CreateTeamsTables extends Migration
 {
@@ -15,28 +16,35 @@ class CreateTeamsTables extends Migration
         // Create Teams Table...
         Schema::create('teams', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('owner_id')->index();
+            $table->integer('owner_id')->unsigned()->nullable()->index();
             $table->string('name');
             $table->timestamps();
+
+            $table->foreign('owner_id')->references('id')->on('users')->onDelete('cascade');
         });
 
         // Create User Teams Intermediate Table...
         Schema::create('user_teams', function (Blueprint $table) {
-            $table->integer('team_id');
-            $table->integer('user_id');
+            $table->integer('team_id')->unsigned();
+            $table->integer('user_id')->unsigned();
             $table->string('role', 25);
 
             $table->unique(['team_id', 'user_id']);
+            $table->foreign('team_id')->references('id')->on('teams')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
 
         // Create Invitations Table...
         Schema::create('invitations', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('team_id')->index();
-            $table->integer('user_id')->nullable()->index();
+            $table->integer('team_id')->unsigned()->index();
+            $table->integer('user_id')->unsigned()->nullable()->index();
             $table->string('email');
             $table->string('token', 40)->unique();
             $table->timestamps();
+
+            $table->foreign('team_id')->references('id')->on('teams')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
@@ -47,6 +55,17 @@ class CreateTeamsTables extends Migration
      */
     public function down()
     {
+        Schema::table('teams', function (Blueprint $table) {
+            $table->dropForeign('teams_owner_id_foreign');
+        });
+        Schema::table('user_teams', function (Blueprint $table) {
+            $table->dropForeign('user_teams_team_id_foreign');
+            $table->dropForeign('user_teams_user_id_foreign');
+        });
+        Schema::table('invitations', function (Blueprint $table) {
+            $table->dropForeign('invitations_team_id_foreign');
+            $table->dropForeign('invitations_user_id_foreign');
+        });
         Schema::drop('teams');
         Schema::drop('user_teams');
         Schema::drop('invitations');
